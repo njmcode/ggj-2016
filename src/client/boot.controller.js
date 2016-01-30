d6e4df77a7ec48f5ad98ab783b11fb6cd88a1c81
Neil McCallion <neil@rehabstudio.com>
2016-01-30 19:08:20 +0000

    Adding controller files

---
+++ src/client/boot.controller.js
@@ -0,0 +1,38 @@
+/**
+ * boot.controller.js
+ * Entrypoint for controller-specific logic.
+**/
+
+/*
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('./sw.js')
+           .then(function() { console.log("ServiceWorker registered"); });
+}
+*/
+
+var CONFIG = require('./js/config');
+
+var States = {
+    'Controller': require('./js/states/controller')
+};
+
+/**
+ * Main app to bootstrap the controller.
+**/
+function Main(){
+    // Create a new game
+    var game = new Phaser.Game(
+        CONFIG.gameSize.width,
+        CONFIG.gameSize.height,
+        Phaser.AUTO
+    );
+
+    for(var k in States) {
+        game.state.add(k, States[k]);
+    }
+
+    game.state.start('Controller');
+};
+
+Main();
