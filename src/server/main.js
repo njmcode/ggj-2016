2a6da05347bd0dae2178a54104b1b68f4765b986
Ryan Grieve <ryan@rehabstudio.com>
2016-01-30 15:54:12 +0000

    adding basic server with sockets, adding linter

---
+++ src/server/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var http = require('http');
+var express = require('express');
+var socketio = require('socket.io');
+var handlebars = require('express-handlebars');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+app.engine('.hbs', handlebars({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+var globals = {
+    connections: []
+};
+
+
+var httpHandlers = {
+
+    index: function(req, res) {
+        res.render('index');
+    }
+
+};
+
+app.get('/', httpHandlers.index);
+
+
+io.on('connection', function(socket) {
+
+    var socketHandlers = {
+
+        join: function() {
+            globals.connections.push(socket);
+            socket.emit('connected');
+        },
+        disconnect: function() {
+            globals.connections.splice(globals.connections.indexOf(socket), 1);
+        }
+
+    };
+
+    socket.on('join', socketHandlers.join);
+    socket.on('disconnect', socketHandlers.disconnect);
+
+});
+
+
+server.listen(5000, function() {
+    console.log('Server up and listening for connections...');
+});
