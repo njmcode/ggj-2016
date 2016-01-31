04d6106a224cb6936838614ad1772cdd5f2f6952
Ryan Grieve <ryan@rehabstudio.com>
2016-01-31 11:32:16 +0000

    wizards and towers

--- src/client/js/actors/wizard.js
+++ src/client/js/actors/wizard.js
@@ -1,21 +1,44 @@
+'use strict';
 
-var CONFIG = require('../config');
-
-var Wizard = function(state) {
-	// Inherit from Phaser's Sprite class
-    Phaser.Sprite.call(this, state.game, 0, 0, 's-wizard', 0);
-
-    // Set anchor point so the pointer finger of the graphic matches
-    // that of the regular cursor
-    this.anchor.setTo(0.5, 1);
+var Wizard = function(state, color, x, y) {
+    Phaser.Graphics.call(this, state.game, x, y);
     this.game = state.game;
+    this.animState = 'idle';
+    this.color = color;
 };
 
-Wizard.prototype = Object.create(Phaser.Sprite.prototype);
+Wizard.prototype = Object.create(Phaser.Graphics.prototype);
 Wizard.prototype.constructor = Wizard;
 
-Wizard.prototype.update = function() {
+Wizard.prototype.pointStates = {
+    idle: [
+        [30, 50], // neck
+        [20, 20], // head left
+        [40, 10], // head right
+        [30, 50], // neck
+        [30, 70], // chest
+        [20, 70], // left hand
+        [30, 70], // chest
+        [40, 70], // right hand
+        [30, 70], // chest
+        [30, 90], // crotch
+        [20, 120], // left foot
+        [30, 90], // crotch
+        [40, 120] // right foot
+    ]
+};
 
+
+Wizard.prototype.render = function() {
+    var rand = function() {
+        return (Math.random() - 0.5) * 10;
+    }
+    this.clear();
+    this.lineStyle(2, this.color);
+    var points = this.pointStates[this.animState];
+    points.forEach(function(pnt) {
+        this.lineTo(pnt[0] + rand(), pnt[1] + rand());
+    }, this);
 };
 
 module.exports = Wizard;
