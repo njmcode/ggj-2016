b305dcd6c703ca6d7a50409495a11964783610fc
Daniel Porter <dporter@proofpoint.com>
2017-01-26 01:59:08 +0000

    Use WebFont from npm and bundle

--- client/boot.js
+++ client/boot.js
@@ -5,22 +5,14 @@
  * Entrypoint of the app. Initializes the main app.
 **/
 
+var WebFont = require('webfontloader');
 var Main = require('./js/main');
 
-window.WebFontConfig = {
+WebFont.load({
     google: {
-        families: [
-            'VT323::latin'
-        ]
+        families: ['VT323::latin']
     },
     active: function() {
         new Main();
     }
-};
-
-var wf = document.createElement('script');
-wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-wf.type = 'text/javascript';
-wf.async = 'true';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(wf, s);
+});
